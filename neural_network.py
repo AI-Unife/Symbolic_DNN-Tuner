4919d7d5d3670a21d0a26b3df4a80bd645b80b9b
AndreaFilippini <56972407+AndreaFilippini@users.noreply.github.com>
2024-07-09 23:09:23 +0200

    Partial documentation of neural network

--- neural_network.py
+++ neural_network.py
@@ -21,7 +21,17 @@ from LOLR import Lolr
 from search_space import search_space
 
 class neural_network:
+    """
+    class used for the management of the neural network architecture,
+    offering methods for training the dnn and adding and removing convolutional layers
+    """
     def __init__(self, X_train, Y_train, X_test, Y_test, n_classes):
+        """
+        initialises the attributes of the class.
+        first part is used for storing the examples of the dataset,
+        the second part to keep track of the number of the various parts of the dnn,
+        for example the number of convolutional or dense layers.
+        """
         self.train_data = X_train
         self.train_labels = Y_train
         self.test_data = X_test
@@ -31,7 +41,7 @@ class neural_network:
         self.train_data /= 255
         self.test_data /= 255
         self.n_classes = n_classes
-        self.epochs = 200
+        self.epochs = 2
         self.last_dense = 0
         self.counter_fc = 0
         self.counter_conv = 0
@@ -43,11 +53,14 @@ class neural_network:
 
     def build_network(self, params, new):
         """
-        Function for define the network structure
-        :return: model
+        Function to define the network structure
+        :param params new: network layer parameters
+        :return: built model
         """
-        
+        # try to resume the training of the last network, otherwise create a new one
         try:
+            # sorts the models in the dedicated directory and try to open the last one created
+            # if this is successful, read the model in json format and deserialize it in keras format
             list_ckpt = os.listdir("Model")
             list_ckpt.sort()
             f = open("Model/" + list_ckpt[len(list_ckpt)-1])
@@ -57,6 +70,7 @@ class neural_network:
             model = tf.keras.models.model_from_json(model_json)
             print("MODELLO PRECEDENTE")
         except:
+            # if the last trained model cannot be loaded, create a new neural network
             print(self.train_data.shape)
 
             inputs = Input((self.train_data.shape[1:]))
@@ -128,6 +142,7 @@ class neural_network:
         network_dict['new_output_tensor_of'].update(
             {model.layers[0].name: model.input})
 
+        
         d = network_dict['input_layers_of']
         k = []
         _d = {}
@@ -223,13 +238,14 @@ class neural_network:
     def remove_conv_layer(self, model, params):
         """
         Remove convolutional layer
-        Args:
-            model (_type_): keras model
-
-        Returns:
-            new_model : new updated keras model
+        :param model (_type_): keras model
+        :return: new_model : new updated keras model
         """
+        # check if there's at least one batch normalization operation in the network
         btc = any(['batch' in i.name for i in model.layers])
+
+        # count the number of convolutional layers in the network and if there is only one,
+        # return the model without removing further layers
         c = 0
         layers_list = model.layers
         for i in layers_list:
@@ -237,19 +253,37 @@ class neural_network:
                 c += 1
         if c == 1:
             return model
+
+        # get layer of input
         new_input = model.get_layer(layers_list[0].name)
+
+        # reverse list of layers of the neural network
         reverse_layers_list = layers_list[::-1]
+
+        # list used to map the final network structure after the removal of the conv layer
         buffer_rev = reverse_layers_list
+
+        # init layers accumulators to empty lists
         reused_layers, to_delete, head = [], [], []
+
+        # iterate over each layer of the dnn
         for layer in reverse_layers_list:
+            # if the current layer is a convolutional layer
             if 'conv' in layer.name:
+                # if there's at least one batch normalization in the network
                 if btc:
+                    # last part of the network will start with flatten
+                    # remove dropout temporary from the architeture
                     to_delete.append(buffer_rev[reverse_layers_list.index(layer)-3])
                     head_start = buffer_rev[reverse_layers_list.index(layer)-4]
                     buffer_rev.remove(buffer_rev[reverse_layers_list.index(layer)-3])
                 else:
+                    # last part of the network will start with flatten
                     head_start = buffer_rev[reverse_layers_list.index(layer)-3]
-
+ 
+                # insert layers that need to be deleted into to_delete list
+                # and remove them from the buffer_rev that maps the dnn architeture
+                # specifically the convolutional layer, the activation and the batch norm
                 to_delete.append(buffer_rev[reverse_layers_list.index(layer)-2])
                 to_delete.append(buffer_rev[reverse_layers_list.index(layer)-1])
                 to_delete.append(buffer_rev[reverse_layers_list.index(layer)])
@@ -257,27 +291,37 @@ class neural_network:
                 buffer_rev.remove(buffer_rev[reverse_layers_list.index(layer)-1])
                 buffer_rev.remove(buffer_rev[reverse_layers_list.index(layer)])
                 break
+        
+        # iterate over each layer
         for layer in buffer_rev[:-1]:
+            # if the name of the first layer of the final part of the dnn is present in the buffer
             if head_start.name in layer.name:
+                # put in the head part all layers that have been reused and the layer itself
                 head.extend(reused_layers)
                 reused_layers = []
                 head.append(buffer_rev[reverse_layers_list.index(layer)])
             else:
+                # otherwise accumulate the layers in the list of reused layers
                 reused_layers.append(buffer_rev[reverse_layers_list.index(layer)])
         
+        # return the model if there's only one conv layer in the neural network
         if c == 1:
             return model
+
+        # reverse layers accumulators in order to build the new dnn architeture
         head.reverse()
         to_delete.reverse()
         reused_layers.reverse()
         
+        # print result of the dnn architeture analysis, showing the head of the dnn and the reused layers
         print("\n\n### head ###")
         for i in head:
             print(i.name)
         print("\n\n### reuse ###")
         for i in reused_layers:
             print(i.name)
-            
+
+        # generates the neural network by iterating over the previously constructed lists, starting from the input 
         x = new_input.output
         buff = None
         for e, i in enumerate(reused_layers):
@@ -286,6 +330,9 @@ class neural_network:
                     if 'max_pool' in i.name or 'activation' in i.name or 'dropout' in i.name:
                         pass
                 else:
+                    # get current layer, the last one of the reused layer,
+                    # and put its output as the last of the second deleted element (activation)
+                    # this allows continuity of output size once the conv layer is removed
                     _x = model.get_layer(i.name)
                     _x._outbound_nodes[0] = to_delete[1]._outbound_nodes[0]
                     x = _x(x)
@@ -294,10 +341,13 @@ class neural_network:
                     if 'max_pool' in i.name or 'activation' in i.name or 'dropout' in i.name or 'batch' in i.name:
                         pass
                 else:
+                    # concatenates the various layers based on reused layer list
                     x = model.get_layer(i.name)(x)
             buff = i
     
+        # add the last part to the neural network architecture
         for e, i in enumerate(head):
+            # based on the layer name, add a a layer with the same type
             if 'dense' in i.name:
                 x = Dense(i.units, name='dense_{}'.format(time()))(x)
             elif 'dropout' in i.name:
@@ -310,11 +360,15 @@ class neural_network:
             elif 'batch' in i.name:
                 x = BatchNormalization(name=i.name)(x)
             else:
+                # if the layer is an activation and the iteration 
+                # reached the last element of the list, add the output layer with softmax
+                # otherwise if it's a generic activation, add it to the network
                 if 'activation' in i.name and e == len(head)-1:
                     x = Activation('softmax', name='activation_{}'.format(time()))(x)
                 else:
                     x = Activation(params['activation'], name='activation_{}'.format(time()))(x)
         
+        # depending on the keras version, it's necessary to determine where to find the input tensor
         new_input_model = new_input._input_tensor if hasattr(new_input, '_input_tensor') else new_input.input             
         return Model(inputs=new_input_model, outputs=x)
         
@@ -322,26 +376,33 @@ class neural_network:
     def training(self, params, new, new_fc, new_conv, rem_conv, da, space):
         """
         Function for compiling and running training
-        :return: training history
+        :param params, new, new_fc, new_conv, rem_conv, da, space: parameters to indicate a possible operation on the network structure and hyperparameter search space
+        :return: training history, trained model and and performance evaluation score 
         """
 
+        # build neural network
         model = self.build_network(params, new)
         try:
+            # try adding or removing a layer in the neural network based on the anomalies diagnosis
             if new or new_fc or new_conv or rem_conv:
+                # if the flag for the addition of a dense layer is true
                 if new_fc:
                     if new_fc[0]:
                         self.dense = True
                         model = self.insert_layer(model, '.*dense.*', params, num_fc=new_fc[1])
+                # if the flag for the addition of regularization is true
                 if new:
                     self.rgl = True
                     self.dense = False
                     model = self.insert_layer(model, '.*activation.*', params)
+                # if the flag for the addition of a convolutional layer
                 if new_conv:
                     if new_conv[0]:
                         self.conv = True
                         self.dense = False
                         self.rgl = False
                         model = self.insert_layer(model, '.*flatten.*', params, num_cv=new_conv[1], position='before')
+                # if the flag for the removal of a convolutional layer is true
                 if rem_conv:
                     self.conv = False
                     self.dense = False
@@ -350,6 +411,8 @@ class neural_network:
         except Exception as e:
             print(colors.FAIL, e, colors.ENDC)
         
+        # print the structure of the neural network and save it in a json file,
+        # using the current time as identifier of the model
         print(model.summary())
         model_name_id = time()
         model_json = model.to_json()
@@ -357,8 +420,10 @@ class neural_network:
         with open(model_name, 'w') as json_file:
             json_file.write(model_json)
 
+        # save the id of the model in an attribute, so that it can be used later to save an associated db with the same id
         self.last_model_id = model_name_id
 
+        # try to load a set of weights 
         try:
             model.load_weights("Weights/weights.h5")
         except:
@@ -378,8 +443,10 @@ class neural_network:
         es1 = EarlyStopping(monitor='val_loss', min_delta=0.005, patience=15, verbose=1, mode='min')
         es2 = EarlyStopping(monitor='val_accuracy', min_delta=0.005, patience=15, verbose=1, mode='max')
         reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, verbose=1, min_lr=1e-4)
-
+ 
+        # if the flag of data augmentation is true
         if da:
+            # define a generator in which are present the values of the data augmentation parameters
             datagen = ImageDataGenerator(
                 width_shift_range=0.1,
                 height_shift_range=0.1,
@@ -388,18 +455,23 @@ class neural_network:
                 horizontal_flip=True)
             datagen.fit(self.train_data)
 
+            # train the model
             history = model.fit(
                 datagen.flow(self.train_data, self.train_labels, batch_size=params['batch_size']), epochs=self.epochs,
                 verbose=1, validation_data=(self.test_data, self.test_labels),
                 callbacks=[tensorboard, reduce_lr, es1, es2]).history
         else:
-
+            # train the network without data augmentation
             history = model.fit(self.train_data, self.train_labels, epochs=self.epochs, batch_size=params['batch_size'],
                                 verbose=1,
                                 validation_data=(self.test_data, self.test_labels),
                                 callbacks=[tensorboard, reduce_lr, es1, es2]).history
 
+        # evaluates model performance on test data
         score = model.evaluate(self.test_data, self.test_labels)
+
+        # save the neural network weights and then reload them from the same json file you just saved
+        # this avoids errors because of the changes in the network structure before training
         weights_name = "Weights/weights-{}.weights.h5".format(model_name_id)
         model.save_weights(weights_name)
 
