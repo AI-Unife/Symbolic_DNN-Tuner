5a51bc1ba4e181b582504617ba48420ce41fab26
AndreaFilippini <56972407+AndreaFilippini@users.noreply.github.com>
2024-07-26 20:50:43 +0200

    Fix and documentation of new methods for net structure

--- dynamic_net.py
+++ dynamic_net.py
@@ -11,6 +11,8 @@ class dynamic_net:
     def remove_section(self, model, target, linked_layers, delimiter, first_found):
         """
         method used to remove a section of layers
+        :param model target linked_layers delimiter first_found: model from which to remove linked_layers starting from the target
+        :return: new model without the linked_layers
         """
 
         # boolean used to identify which layers in the new architecture can use the old weights
@@ -66,8 +68,12 @@ class dynamic_net:
     def insert_section(self, model, n_section, new_section, position, target):
         """
         method used for inserting a new section of layers
+        :param model, n_section, new_section, position, target: model from which to insert new_section starting from the target in a certain position
+        :return: new model with new_section
         """
 
+        # check if the list contains instances of keras layers,
+        # and if this is not true, return the old model
         if not self.all_layers(new_section):
             print("\n#### New section contains elements that are not layers ####\n")
             return model
@@ -107,6 +113,8 @@ class dynamic_net:
 
             current_layer = {i.name : [reused_weights, {'class_name' : layer_class}, i.get_config()]}
 
+            # Depending on the value of the position, insert the new section
+            # before, after, or replace the target layers
             if position == 'before':
                 net_dense |= (section | current_layer)
             elif position == 'after':
@@ -117,12 +125,13 @@ class dynamic_net:
             else:
                net_dense |= current_layer
         
-        return self.model_from_dict(model, net_dense)
-        #return self.build_model(model, net_dense)
+        return self.build_model(model, net_dense)
 
     def build_model(self, model, model_dict):
         """
         Method used to build the network and handle any problems during its creation
+        :param model model_dict: with the old model and the dict, build the new model and handle the errors
+        :return: new model
         """
         try:
             return self.model_from_dict(model, model_dict)
@@ -133,6 +142,8 @@ class dynamic_net:
     def model_from_dict(self, model, model_dict):
         """
         method used to build the network based on a dict containing the configurations of each layer
+        :param model, model_dict: starting from the old model and the new dictionary, build the new architecture
+        :return: new model
         """
 
         # set variables containing respectively the new network archiecture and input layer to 'None'
@@ -150,18 +161,23 @@ class dynamic_net:
             layer = model_dict[layer_key][2]
             layer_name = model_dict[layer_key][1]['class_name']
 
+            # if the current layer in the dictionary is the input layer, initialize the input of the new model
             if 'Input' in layer_name:
                 input_shape = model.input
+ 
+                # fix for keras >= 3.4, the input layer is saved
+                # in lists of lists, instead of a single list
                 if isinstance(input_shape, list):
                     input_shape = input_shape[0]
 
-                new_inputs = Input(input_shape.shape[1:])
+                new_inputs = input_shape
                 x = new_inputs
             elif model_dict[layer_key][0] and layer_key in name_list:
                 # if i can use old weights from the current layer, load them from the model
                 x = model.get_layer(layer['name'])(x)
             else:
-                # otherwise create a new layer based on the configuration of the previously saved one
+                # for each layer in the model, instantiate a dummy layer of the same class with getattr,
+                # and then load the correct values from the configuration saved during the dict construction
                 if layer_name in ['Conv2D', 'SeparableConv2D', 'Conv2DTranspose']:
                     layer_inst = getattr(module, layer_name)(1, 1)
                 elif layer_name in ['ZeroPadding2D',
@@ -178,6 +194,7 @@ class dynamic_net:
                 elif layer_name in ['Flatten','BatchNormalization', 'ReLU', 'Softmax']:
                     layer_inst = getattr(module, layer_name)()
 
+                # restore the correct values and add the layer to the model
                 x = layer_inst.from_config(layer)(x)
 
         return Model(inputs=new_inputs, outputs=x)
@@ -185,10 +202,14 @@ class dynamic_net:
     def add_names(self, layer_list):
         """
         method used to generate an id to be added to each new layer name
+        :param layer_list: list of layers to which add the identifier
+        :return: layer list with new names
         """
         naming = "_{}".format(time() + random())
         new_list = []
         for layer in layer_list:
+            # change the name in the layer configuration,
+            # adding an id given by time and a random value
             layer_config = layer.get_config()
             layer_config['name'] += naming
             new_list += [layer.from_config(layer_config)]
@@ -197,13 +218,18 @@ class dynamic_net:
     def get_last_section(self, model, type_class):
         """
         method used to find the the name of the first layer of a specific type section
+        :param model type_class: search the name of the first layer of the last section in the model with 'type_class' as type
+        :return: name of the layer where the searched section begins
         """
    
         # get the layers of the model and reverse the list
         layer_list = model.layers
         layer_list = layer_list[::-1]
 
+        # name of the layer where the searched section begins
         type_name = None
+
+        # boolean to identify the beginning of the section
         type_flag = False
 
         # iterate over each layer in reverse order
@@ -228,14 +254,26 @@ class dynamic_net:
     def all_layers(self, layer_list):
         """
         method used to check if all elements of a list are layers
+        :param layer_list: list of input layers
+        :return: boolean indicating if all elements are keras layers
         """
         return all(['.layers.' in str(type(i)) for i in layer_list])
 
     def count_layer_type(self, model, type):
+        """
+        method used to count how many layers of a certain type are in the model
+        :param  model type: model in which to count number of 'type' layers
+        :return: number of layers of the specified type
+        """
         return [(i.__class__.__name__ == type) for i in model.layers].count(True)
 
     def any_batch(self, model):
-        return any(['batch' in i.name for i in model.layers])
+        """
+        method used to check if at least one batchNorm operation is in the model
+        :param model: model in which to detect the presence of batchNormalizations
+        :return: boolean indicating if there's at least one batchNormalization
+        """
+        return any(['BatchNormalization' in i.__class__.__name__ for i in model.layers])
 
 if __name__ == '__main__':
 
@@ -260,6 +298,13 @@ if __name__ == '__main__':
     model = dynamicNet.insert_section(model, 1, new_section, 'after', last_max)
     model.summary()
 
+
+    print(dynamicNet.any_batch(model))
+    new_section = [BatchNormalization(), AveragePooling2D((2,2))]
+    model = dynamicNet.insert_section(model, 1, new_section, 'replace', 'MaxPooling2D')
+    model.summary()
+
+    # remove last convolutional section
     last_conv = dynamicNet.get_last_section(model, 'Conv2D')
     linked_list = ['Conv2D', 'Activation', 'MaxPooling2D']
     model = dynamicNet.remove_section(model, last_conv, linked_list, True, False)
@@ -269,7 +314,7 @@ if __name__ == '__main__':
     quit()
 
     new_section = [Conv2D(256, (2,2), padding="same"), Activation('relu')]
-    model = dynamicNet.insert_section(model, 2, new_section, 'after', 'input')
+    model = dynamicNet.insert_section(model, 2, new_section, 'after', 'Input')
     last_act = dynamicNet.get_last_section(model, 'Activation')
     model = dynamicNet.insert_section(model, 1, [MaxPooling2D()], 'after', last_act)
     model.summary()
--- neural_network.py
+++ neural_network.py
@@ -109,59 +109,114 @@ class neural_network:
         return model
 
     def insert_conv_section(self, model, params, n_conv):
+        """
+        method used for inserting a convolutional section
+        :param model params n_conv: insert in 'model' a number of 'n_conv' sections with 'params' parameters
+        :return: model with new conv section
+        """
+
+        # if the number of convolutions is greater than or equal to the max, then return the model
         if self.dnet.count_layer_type(model, 'Conv2D') >= self.tot_conv:
             return model
 
+        # build the new convolutional section, consisting of a convolution and its activation
         new_section = [Conv2D(params['unit_c2'], (3,3)), Activation(params['activation'])]
+
+        # if batchNormalization is already in the model, add it to the new convolutional section
         if self.dnet.any_batch(model):
             new_section += [BatchNormalization()]
         
+        # insert the new section before the flatten, so after the last convolutional section
         return self.dnet.insert_section(model, n_conv, new_section, 'before', 'Flatten')
 
     def insert_batch(self, model, params):
+        """
+        method used for inserting batchNormalization operations
+        :param model params n_conv: insert in 'model' batchNormalization and regularization with 'params' parameters
+        :return: model with batchNormalization and regularization
+        """
+
+        # if batchnormalization is already in the model, then return it
         if self.dnet.any_batch(model):
             return model
 
+        # otherwise add regularization to each convolutional layer
         for layer in model.layers:
             if self.rgl:
                 if 'Conv2D' in layer.__class__.__name__:
                     layer.kernel_regularizer = reg.l2(params['reg'])
 
+        # iterating over all the layers, search all the activations to which to add the batchNormalization
         activation_list = []
         for layer in model.layers:
+            # get the name of the activation function based on the version of keras
             activation_name = layer.activation.__name__ if hasattr(layer, 'activation') else layer.output.name
             layer_class = layer.__class__.__name__
+            # if the activation is not softmax, so the last layer of the network, then save the layer in the list
             if layer_class == 'Activation' and activation_name != 'softmax':
                 activation_list += [layer.name]
-      
+
+        # apply batchNormalization to all saved activations
         return self.dnet.insert_section(model, 1, [BatchNormalization()], 'after', activation_list)
 
     def insert_fc_section(self, model, params, n_fc):
+        """
+        method used for inserting a dense section
+        :param model params n_fc: insert in 'model' a number of 'n_fc' sections with 'params' parameters
+        :return: model with new dense section
+        """
+
+        # if the number of dense layers is greater than or equal to the max, then return the model
         if self.dnet.count_layer_type(model, 'Dense') >= self.tot_fc:
             return model 
 
+        # build the new dense section, consisting of a dense layer and its activation
         new_section = [Dense(params['new_fc']),
                        Activation(params['activation'])]
 
+        # if batchNormalization is already in the model, add it to the new dense section
         if self.dnet.any_batch(model):
             new_section += [BatchNormalization()]
 
+        # add dropout to the dense section
         new_section += [Dropout(params['dr_f'])]
 
+        # insert the new section after the flatten, so a the beggining of the dense section
         return self.dnet.insert_section(model, n_fc, new_section, 'after', 'Flatten')
 
     def remove_conv_section(self, model):
+        """
+        method used for removing a convolutional section
+        :param model: model from which to remove the convolutional section
+        :return: model without convolutional section
+        """
+        # if the number of convolutions is less than or equal to 1,
+        # don't remove any convolution and return the model
         if self.dnet.count_layer_type(model, 'Conv2D') <= 1:
             return model
 
+        # get the name of the first layer of the last convolutional section
         last_conv_start = self.dnet.get_last_section(model, 'Conv2D')
+ 
+        # remove the convolutional section starting from the convolution found earlier
+        # and all associated layers in linked_section
         linked_section = ['Conv2D', 'Activation', 'BatchNormalization', 'MaxPooling2D']
         return self.dnet.remove_section(model, last_conv_start, linked_section, True, True)
 
     def remove_fc_section(self, model):
+        """
+        method used for removing a dense section
+        :param model: model from which to remove the dense section
+        :return: model without dense section
+        """
+
+        # if the number of dense layers is less than or equal to 2,
+        # specifically a dense layer after the flatten and the output,
+        # don't remove any dense layer and return the model
         if self.dnet.count_layer_type(model, 'Dense') <= 2:
             return model
 
+        # remove the first dense section in the model and all associated layers in linked_section
         linked_section = ['Activation', 'BatchNormalization', 'Dropout']
         return self.dnet.remove_section(model, 'Dense', linked_section, True, True)
 
