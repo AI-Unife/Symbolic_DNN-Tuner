6596a0a3856615b08b5673641afde623ab5748ae
Alice Bizzarri <bzzlca@unife.it>
2025-11-05 10:55:31 +0100

    add test quantizer

--- quantizer/quantizer_interface.py
+++ quantizer/quantizer_interface.py
@@ -6,15 +6,31 @@ class quantizer_interface(ABC):
     The ABC module is used to define an infrastructure for the definition of abstract classes.
     Each method marked with @abstractmethod must be implemented by modules.
     """
-            
+    
+    
     @abstractmethod
     def quantizer_function(self):
         """
         Function to quantize the model
-        :return: quantized model, score
+        :return: quantized model
         """
         pass
     
+    @abstractmethod
+    def evaluate_quantized_model(self, x_test, y_test):
+        """
+        Function to evaluate the quantized model
+        :return: score
+        """
+        pass
+    
+    @abstractmethod
+    def save_quantized_model(self):
+        """
+        Function to save the quantized model
+        :param path: path to save the quantized model
+        """
+        pass
 
     @abstractmethod
     def printing_values(self):
